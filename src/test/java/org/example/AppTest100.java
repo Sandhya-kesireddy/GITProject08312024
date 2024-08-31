d0f1046037994c837e12e36f50e7e3a0e285032c
sandhyak <sandhya_kesireddy@epam.com>
2024-08-31 14:42:46 +0530

    First commit

---
+++ src/test/java/org/example/AppTest100.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest100 {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+    @Test
+    public void myTest(){
+        System.out.println("My First Method");
+        
+    }
+}
